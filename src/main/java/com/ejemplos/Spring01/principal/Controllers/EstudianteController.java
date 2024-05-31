a00d3bf9ddbb6fa43fcc22bb91fd09b5ba0d7650
WinTux <rusok.music@gmail.com>
2024-05-31 19:31:46 -0400

    Ultima clase

--- src/main/java/com/ejemplos/Spring01/principal/Controllers/EstudianteController.java
+++ src/main/java/com/ejemplos/Spring01/principal/Controllers/EstudianteController.java
@@ -5,11 +5,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,6 +32,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ejemplos.Spring01.principal.Exceptions.EstudianteNoEncontradoException;
 import com.ejemplos.Spring01.principal.Models.Est;
@@ -67,24 +70,49 @@ public class EstudianteController {
 		//return new ResponseEntity<>(estudiantes.values(),HttpStatus.OK);
 	}
 	@PostMapping("/estudiante")//localhost:7001/estudiante [POST]
-	public ResponseEntity<Object> nuevoEst(@RequestBody Estudiante est){
-		estudiantes.put(est.getId()+"", est);
-		return new ResponseEntity<>("Se creó el estudiante "+est.getId(), HttpStatus.CREATED);
+	public ResponseEntity<Object> nuevoEst(@RequestBody Est est){
+		estudianteService.registrar(est);
+		URI ubicacionDelRecurso = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(est.getMatricula())
+				.toUri();
+		//estudiantes.put(est.getId()+"", est);
+		return ResponseEntity.created(ubicacionDelRecurso).build();
 	}
 	@GetMapping("/estudiante/estado")//localhost:7001/estudiante/estado [GET]
 	public ResponseEntity<String> getEstado(){
 		return ResponseEntity.ok("Controlador de estudiante funcionando.");
 	}
 	@PutMapping("/estudiante/{id}")//localhost:7001/estudiante/5 [PUT]
-	public ResponseEntity<Object> modificarEstudiante(@PathVariable("id") int id, @RequestBody Estudiante est){
+	public ResponseEntity<Object> modificarEstudiante(@PathVariable("id") int id, @RequestBody Est est){
+		Optional<Est> estudiante = estudianteService.hallarEstudiante(id);
+		if(!estudiante.isPresent())
+			throw new EstudianteNoEncontradoException();
+		Est estDDBB = estudiante.get();
+		estDDBB.setNombre(est.getNombre());
+		estDDBB.setApellido(est.getApellido());
+		estDDBB.setFechanacimiento(est.getFechanacimiento());
+		estDDBB.setCarreraid(est.getCarreraid());
+		estDDBB.setPassword(est.getPassword());
+		estDDBB.setEmail(est.getEmail());
+		estDDBB.setEstado(est.getEstado());
+		estudianteService.registrar(estDDBB);
+		/*
 		estudiantes.remove(id);
 		est.setId(id);
 		estudiantes.put(id+"", est);
+		*/
 		return new ResponseEntity<>("Se actualizaron los datos del estudiante "+id, HttpStatus.OK);
 	}
 	@DeleteMapping("/estudiante/{id}")//localhost:7001/estudiante/2 [DELETE]
 	public ResponseEntity<Object> elminarEstudiante(@PathVariable("id") int id){
-		estudiantes.remove(id+"");
+		Optional<Est> estudiante = estudianteService.hallarEstudiante(id);
+		if(!estudiante.isPresent())
+			throw new EstudianteNoEncontradoException();
+		Est estDDBB = estudiante.get();
+		estudianteService.eliminar(estDDBB);
+		//estudiantes.remove(id+"");
 		logger.info("Se eliminó correctamente "+ id);
 		return new ResponseEntity<>("Se eliminó al estudiante "+id, HttpStatus.OK);
 	}
--- src/main/java/com/ejemplos/Spring01/principal/Services/EstudianteService.java
+++ src/main/java/com/ejemplos/Spring01/principal/Services/EstudianteService.java
@@ -1,6 +1,7 @@
 package com.ejemplos.Spring01.principal.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,15 @@ public class EstudianteService {
 	}
 	public List<Est> getPorApellido(String apellido){
 		return estudianteRepositorio.findByApellido(apellido);
+	}
+	public void registrar(Est est) {
+		System.out.println("Se registró al estudiante "+est.getNombre());
+		estudianteRepositorio.save(est);
+	}
+	public Optional<Est> hallarEstudiante(int matricula){
+		return estudianteRepositorio.findById(matricula);
+	}
+	public void eliminar(Est est) {
+		estudianteRepositorio.delete(est);
 	}
 }
